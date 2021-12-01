c9828dd013e268fc798bee5784a49667a50b13e3
Rose <rosemwangi305@gmail.com>
2021-12-01 12:18:04 +0300

    render leads from leadsFromLocalStorage

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
+});
 
 function renderLeads() {
-    let listItems = ""
+    let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
@@ -18,7 +30,7 @@ function renderLeads() {
                     ${myLeads[i]}
                 </a>
             </li>
-        `
+        `;
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems;
 }
